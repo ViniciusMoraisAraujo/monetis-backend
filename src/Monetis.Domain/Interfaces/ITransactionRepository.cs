d1e926393267279c5716b430976b3f9ca61d4424
Vinicius Araujo <vinidev.morais@gmail.com>
2026-03-16 13:20:00 -0300

    feat(infra): add find methods to TransactionRepository

--- src/Monetis.Domain/Interfaces/ITransactionRepository.cs
+++ src/Monetis.Domain/Interfaces/ITransactionRepository.cs
@@ -1,7 +1,12 @@
 ﻿using Monetis.Domain.Entities;
+using Monetis.Domain.Enums;
 
 namespace Monetis.Domain.Interfaces;
 public interface ITransactionRepository : IRepository<Transaction>
 {
-    
+    Task<IEnumerable<Transaction>> GetByCategoryAsync(Guid categoryId);
+    Task <IEnumerable<Transaction>> GetByTypeAsync(TransactionType type);
+    Task <IEnumerable<Transaction>> GetByUserAsync(Guid userId);
+    Task<IEnumerable<Transaction>> GetAllByPeriodAsync(DateTime startDate, DateTime endDate, bool descending);
+    Task<IEnumerable<Transaction>> GetAllByStatusAsync(TransactionStatus status);
 }
--- src/Monetis.Infrastructure/Repository/TransactionRepository.cs
+++ src/Monetis.Infrastructure/Repository/TransactionRepository.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Monetis.Domain.Entities;
+using Monetis.Domain.Enums;
 using Monetis.Domain.Interfaces;
-using Monetis.Infrastructure.Data;
+using Monetis.Infrastructure.Data; 
 
 namespace Monetis.Infrastructure.Repository;
 
 public class TransactionRepository(MonetisDataContext context) : BaseRepository<Transaction>(context),ITransactionRepository
 {
-    
+    public async Task<IEnumerable<Transaction>> GetByCategoryAsync(Guid categoryId)
+    {
+        return await context.Transactions.AsNoTracking().Where(x => x.CategoryId == categoryId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByTypeAsync(TransactionType type)
+    {
+        return await context.Transactions.AsNoTracking().Where(x => x.Type == type).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByUserAsync(Guid userId)
+    {
+        return  await context.Transactions.AsNoTracking().Where(x => x.UserId == userId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> GetAllByPeriodAsync(DateTime startDate, DateTime endDate, bool descending)
+    {
+        var query =  context.Transactions.AsNoTracking()
+            .Where(x => x.CreatedAt >= startDate && x.CreatedAt <= endDate);
+
+        query = descending
+            ? query.OrderByDescending(x => x.CreatedAt)
+            : query.OrderBy(x => x.CreatedAt);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> GetAllByStatusAsync(TransactionStatus status)
+    {
+        return await context.Transactions.AsNoTracking().Where(x => x.Status == status).ToListAsync();
+    }
 }
