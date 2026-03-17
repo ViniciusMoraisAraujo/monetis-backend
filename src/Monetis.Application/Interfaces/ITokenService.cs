1f988407631bdbda8c615e0db604bdfa6b48fd92
Vinicius Araujo <vinidev.morais@gmail.com>
2026-03-17 15:13:32 -0300

    feat(application): create service for passwordhasher

---
+++ src/Monetis.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,6 @@
+﻿namespace Monetis.Application.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateToken(Guid userId, string email);
+}
---
+++ src/Monetis.Infrastructure/Security/TokenService.cs
@@ -0,0 +1,39 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Monetis.Application.Interfaces;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Monetis.Infrastructure.Security;
+
+public class TokenService(IConfiguration configuration) : ITokenService
+{
+    public string GenerateToken(Guid userId, string email)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(2),
+            Issuer = configuration["JwtIssuer"],
+            Audience = configuration["JwtAudience"],
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+}
