997ab4a6338facf67ea3714ab12c6e86139e0dbc
Vinicius Araujo <vinidev.morais@gmail.com>
2026-03-17 15:29:26 -0300

    feat(application): create PasswordHasher

---
+++ src/Monetis.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Monetis.Domain.Entities;
+using Monetis.Domain.Interfaces;
+
+namespace Monetis.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    private readonly PasswordHasher<User> _hasher = new();
+
+    public string Hash(string password)
+    {
+        return _hasher.HashPassword(null!, password);
+        
+    }
+
+    public bool Verify(string password, string passwordHash)
+    {
+        var result = _hasher.VerifyHashedPassword(null!, passwordHash, password);
+        return result != PasswordVerificationResult.Failed;
+    }
+}
