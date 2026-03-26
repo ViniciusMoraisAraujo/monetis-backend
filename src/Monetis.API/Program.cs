fa8a3cb10c831f2f751778e62e93637f9d0e33d5
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-03-26 13:53:22 -0300

    feat(api): add Swagger UI and OpenAPI documentation

--- src/Monetis.API/Program.cs
+++ src/Monetis.API/Program.cs
@@ -5,6 +5,10 @@ using Monetis.Application;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
@@ -13,7 +17,11 @@ builder.Services.AddMemoryCache();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 app.UseMiddleware<RateLimitingMiddleware>();
