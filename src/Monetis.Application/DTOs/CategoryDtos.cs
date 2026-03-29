45600049c085dc22aaf979867185d0a4a06098c1
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-03-29 16:18:00 -0300

    refactor: remove unused parameters in CategoryDtos.cs

--- src/Monetis.Application/DTOs/CategoryDtos.cs
+++ src/Monetis.Application/DTOs/CategoryDtos.cs
@@ -2,6 +2,6 @@ using Monetis.Domain.Enums;
 
 namespace Monetis.Application.DTOs;
 
-public record CategoryDto(Guid Id, string Name, Guid UserId, TransactionType Type, string Icon);
-public record CreateCategoryDto(string Name, TransactionType Type, string Icon);
+public record CategoryDto(Guid Id, string Name, Guid UserId,  string Icon);
+public record CreateCategoryDto(string Name, string Icon);
 public record UpdateCategoryDto(string Name, string Icon);
