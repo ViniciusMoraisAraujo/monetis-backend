52ff8b0a9866e66254fb21f47d55092bb42178a1
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-03-28 22:27:01 -0300

    refactor: remove unused methods of ICategoryRepository.cs

--- src/Monetis.Domain/Interfaces/ICategoryRepository.cs
+++ src/Monetis.Domain/Interfaces/ICategoryRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Monetis.Domain.Entities;
-using Monetis.Domain.Enums;
 
 namespace Monetis.Domain.Interfaces;
 public interface ICategoryRepository : IRepository<Category>
-{
-    Task<IEnumerable<Category>>  GetAllTransactionTypeReadOnly(TransactionType type);
+{ 
 }
