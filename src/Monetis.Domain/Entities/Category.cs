4e413def98a49ede32c9d51445d09938af3dc0ec
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-03-29 11:15:42 -0300

    refactor: remove unused TransactionType property from Category

--- src/Monetis.Domain/Entities/Category.cs
+++ src/Monetis.Domain/Entities/Category.cs
@@ -7,27 +7,24 @@ public class Category : BaseEntity
     public string Name { get; private set; }
     public Guid? UserId { get; private set; }
     public User? User { get; private set; }
-    public TransactionType Type { get; private set; }
     public string Icon { get; private set; }
     
     protected Category() { }
     
-    public Category(string name, Guid userId, TransactionType type, string icon)
+    public Category(string name, Guid userId,  string icon)
     {
         Name = name;
         UserId = userId;
-        Type = type;
         Icon = icon;
     }
 
-    public static Category CreateSystemCategory(Guid id, string name, TransactionType type, string icon)
+    public static Category CreateSystemCategory(Guid id, string name,  string icon)
     {
         return new Category
         {
             Id = id,
             CreatedAt = new DateTime(2026, 3, 13),
             Name = name,
-            Type = type,
             Icon = icon
         };
     }
