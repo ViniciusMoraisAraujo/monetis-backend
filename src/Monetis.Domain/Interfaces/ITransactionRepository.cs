4c23233008d7414e8816560791b80a7b472fa025
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-03-28 22:26:26 -0300

    refactor: remove unused methods of ITransactionRepository.cs

--- src/Monetis.Domain/Interfaces/ITransactionRepository.cs
+++ src/Monetis.Domain/Interfaces/ITransactionRepository.cs
@@ -3,10 +3,7 @@ using Monetis.Domain.Enums;
 
 namespace Monetis.Domain.Interfaces;
 public interface ITransactionRepository : IRepository<Transaction>
-{
-    Task<IEnumerable<Transaction>> GetByCategoryAsync(Guid categoryId);
-    Task <IEnumerable<Transaction>> GetByTypeAsync(TransactionType type);
-    Task <IEnumerable<Transaction>> GetByUserAsync(Guid userId);
+{ 
+    Task<IEnumerable<Transaction>> GetByUserAsync(Guid userId);
     Task<IEnumerable<Transaction>> GetAllByPeriodAsync(DateTime startDate, DateTime endDate, bool descending);
-    Task<IEnumerable<Transaction>> GetAllByStatusAsync(TransactionStatus status);
 }
