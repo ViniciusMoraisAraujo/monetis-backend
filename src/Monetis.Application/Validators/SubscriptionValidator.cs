2ca7bdaf89790433f4a203f536a561a47e576b32
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-01 22:00:51 -0300

    refacto: better validation in SubscriptionValidator.cs

--- src/Monetis.Application/Validators/SubscriptionValidator.cs
+++ src/Monetis.Application/Validators/SubscriptionValidator.cs
@@ -7,29 +7,37 @@ public class CreateSubscriptionDtoValidator : AbstractValidator<CreateSubscripti
 {
     public CreateSubscriptionDtoValidator()
     {
+        RuleFor(x => x.AccountId)
+            .NotEmpty()
+            .WithMessage("Account is required");
+        
+        RuleFor(x => x.CategoryId)
+            .NotEmpty()
+            .WithMessage("Category is required");
+        
         RuleFor(x => x.Amount)
             .GreaterThan(0)
             .WithMessage("Amount must be greater than zero")
-            .LessThanOrEqualTo(999999.99m)
-            .WithMessage("Amount cannot exceed 999,999.99");
-
+            .LessThanOrEqualTo(9999999999)
+            .WithMessage("Amount cannot exceed 9.999.999.999");
+        
         RuleFor(x => x.Description)
             .NotEmpty()
             .WithMessage("Description is required")
             .MaximumLength(100)
             .WithMessage("Description cannot exceed 100 characters")
-            .Matches(@"^[a-zA-Z0-9\s\-_]+$")
-            .WithMessage("Description can only contain letters, numbers, spaces, hyphens, and underscores");
+            .Matches(@"^[a-zA-ZÀ-ÿ\s0-9\-_]+$")
+            .WithMessage("Description can only contain letters and spaces");
 
         RuleFor(x => x.Frequency)
             .IsInEnum()
-            .WithMessage("Invalid frequency type");
-
+            .WithMessage("Invalid frequency");
+        
         RuleFor(x => x.NextDueDate)
             .NotEmpty()
             .WithMessage("Next due date is required")
-            .Must(x => x > DateTime.UtcNow)
-            .WithMessage("Next due date must be in the future");
+            .Must(x => x >= DateTime.UtcNow)
+            .WithMessage("Next due date cannot be in the past");
     }
 }
 
@@ -40,25 +48,25 @@ public class UpdateSubscriptionDtoValidator : AbstractValidator<UpdateSubscripti
         RuleFor(x => x.Amount)
             .GreaterThan(0)
             .WithMessage("Amount must be greater than zero")
-            .LessThanOrEqualTo(999999.99m)
-            .WithMessage("Amount cannot exceed 999,999.99");
-
+            .LessThanOrEqualTo(9999999999)
+            .WithMessage("Amount cannot exceed 9.999.999.999");
+        
         RuleFor(x => x.Description)
             .NotEmpty()
             .WithMessage("Description is required")
             .MaximumLength(100)
             .WithMessage("Description cannot exceed 100 characters")
-            .Matches(@"^[a-zA-Z0-9\s\-_]+$")
-            .WithMessage("Description can only contain letters, numbers, spaces, hyphens, and underscores");
+            .Matches(@"^[a-zA-ZÀ-ÿ\s0-9\-_]+$")
+            .WithMessage("Description can only contain letters and spaces");
 
         RuleFor(x => x.Frequency)
             .IsInEnum()
-            .WithMessage("Invalid frequency type");
-
+            .WithMessage("Invalid frequency");
+        
         RuleFor(x => x.NextDueDate)
             .NotEmpty()
             .WithMessage("Next due date is required")
-            .Must(x => x > DateTime.UtcNow)
-            .WithMessage("Next due date must be in the future");
+            .Must(x => x >= DateTime.UtcNow)
+            .WithMessage("Next due date cannot be in the past");
     }
 }
