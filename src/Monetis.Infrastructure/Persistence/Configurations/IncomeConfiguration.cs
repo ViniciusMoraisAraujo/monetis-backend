63b07cdf4b385e4412afc33b9379a53eeccd4efa
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-03 18:34:44 -0300

    feat: configure Income entity persistence

--- src/Monetis.Infrastructure/Persistence/Configurations/IncomeConfiguration.cs
+++ src/Monetis.Infrastructure/Persistence/Configurations/IncomeConfiguration.cs
@@ -8,7 +8,35 @@ public class IncomeConfiguration : IEntityTypeConfiguration<Income>
 {
     public void Configure(EntityTypeBuilder<Income> builder)
     {
-        builder.Property(i => i.ReceivedAt)
+        builder.ToTable("Incomes");
+
+        builder.Property(i => i.CategoryId)
             .IsRequired();
+
+        builder.Property(i => i.ReceivedAt)
+            .IsRequired()
+            .HasColumnType("datetime");
+
+        builder.HasIndex(i => i.CategoryId);
+        builder.HasIndex(x => x.AccountId);
+        builder.HasIndex(x => x.UserId);
+
+        builder.HasOne(i => i.Category)
+            .WithMany()
+            .HasForeignKey(i => i.CategoryId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Account)
+            .WithMany()
+            .HasForeignKey(x => x.AccountId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
