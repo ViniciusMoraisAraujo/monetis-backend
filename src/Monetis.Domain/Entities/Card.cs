81bf864c8540d5bde9ee42a7d86de0dd0b5380c9
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-03 17:53:07 -0300

    refactor: rename CreditCard entity to Card

--- src/Monetis.Domain/Entities/Card.cs
+++ src/Monetis.Domain/Entities/Card.cs
@@ -1,13 +1,13 @@
 namespace Monetis.Domain.Entities;
 
-public class CreditCard : BaseEntity
+public class Card : BaseEntity
 {
     public Guid UserId { get; private set; }
     public string Name { get; private set; }
 
-    protected CreditCard() { }
+    protected Card() { }
 
-    public CreditCard(Guid userId, string name)
+    public Card(Guid userId, string name)
     {
         UserId = userId;
         Name = name;
