0f4380c1f15a0d69dc5f81280cf1e35936c151a7
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-03 18:33:26 -0300

    feat(persistence): configure Transfer entity

--- src/Monetis.Infrastructure/Persistence/Configurations/TransferConfiguration.cs
+++ src/Monetis.Infrastructure/Persistence/Configurations/TransferConfiguration.cs
@@ -8,12 +8,38 @@ public class TransferConfiguration : IEntityTypeConfiguration<Transfer>
 {
     public void Configure(EntityTypeBuilder<Transfer> builder)
     {
-        builder.Property(t => t.TransferredAt)
+        builder.ToTable("Transfers");
+
+        builder.Property(t => t.DestinationAccountId)
             .IsRequired();
+
+        builder.Property(t => t.TransferredAt)
+            .IsRequired()
+            .HasColumnType("datetime");
+
+        builder.HasIndex(t => t.DestinationAccountId);
+
+        builder.HasIndex(x => x.AccountId);
+        builder.HasIndex(x => x.UserId);
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Account)
+            .WithMany()
+            .HasForeignKey(x => x.AccountId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
         
         builder.HasOne(t => t.DestinationAccount)
             .WithMany()
             .HasForeignKey(t => t.DestinationAccountId)
+            .IsRequired()
             .OnDelete(DeleteBehavior.Restrict);
+        
+        
     }
 }
