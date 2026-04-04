0feb5121a366bf6fed8f574f60c2e5323cf53da3
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-04 16:17:23 -0300

    feat(domain): enhance User entity with validations and password management
    - Implement private `Validate` method to ensure first name, last name, and email are provided
    - Integrate validation logic into the constructor and `Update` method
    - Add `ChangePassword` method to allow secure updates of the password hash
    - Rename constructor parameter `password` to `passwordHash` for better consistency with the property name
    - Add a mandatory check for the password hash during user creation

--- src/Monetis.Domain/Entities/User.cs
+++ src/Monetis.Domain/Entities/User.cs
@@ -8,18 +8,46 @@ public class User : BaseEntity
     public string PasswordHash { get; private set; }
     
     protected User () { }
-    public User(string firstName, string lastName, string email, string password)
+    
+    public User(string firstName, string lastName, string email, string passwordHash)
     {
+        Validate(firstName, lastName, email);
+        
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new ArgumentException("A senha é obrigatória.");
+
         FirstName = firstName;
         LastName = lastName;
         Email = email;
-        PasswordHash = password;
+        PasswordHash = passwordHash;
     }
 
     public void Update(string firstName, string lastName, string email)
     {
+        Validate(firstName, lastName, email);
+        
         FirstName = firstName;
         LastName = lastName;
         Email = email;
+    }
+
+    public void ChangePassword(string newPasswordHash)
+    {
+        if (string.IsNullOrWhiteSpace(newPasswordHash))
+            throw new ArgumentException("The new password is required.");
+            
+        PasswordHash = newPasswordHash;
+    }
+
+    private void Validate(string firstName, string lastName, string email)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("The name is required.");
+            
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("The last name is required.");
+            
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("The email is required.");
     }
 }
