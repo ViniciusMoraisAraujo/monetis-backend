b7b53bb0fd3fc80a2f3424393af23be238e7002c
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-11 17:04:33 -0300

    feat(persistence): implement CardRepository

---
+++ src/Monetis.Infrastructure/Persistence/Repositories/CardRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Monetis.Domain.Entities;
+using Monetis.Domain.Interfaces;
+using Monetis.Infrastructure.Contexts;
+
+namespace Monetis.Infrastructure.Persistence.Repositories;
+
+public class CardRepository(MonetisDataContext context) : BaseRepository<Card>(context), ICardRepository
+{
+    public async Task<IEnumerable<Card>> GetByUserReadOnlyAsync(Guid userId)
+    {
+        return await context.Cards
+            .AsNoTracking()
+            .Where(x => x.UserId == userId)
+            .ToListAsync();
+    }
+}
