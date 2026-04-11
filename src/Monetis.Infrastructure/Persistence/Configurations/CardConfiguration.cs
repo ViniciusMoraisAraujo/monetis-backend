bc0eb4ba56d1fcbcea4b50b798cda66b3a952f3a
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-11 17:00:25 -0300

    feat(persistence): configure Card entity

---
+++ src/Monetis.Infrastructure/Persistence/Configurations/CardConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Monetis.Domain.Entities;
+
+namespace Monetis.Infrastructure.Persistence.Configurations;
+
+public class CardConfiguration : IEntityTypeConfiguration<Card>
+{
+    public void Configure(EntityTypeBuilder<Card> builder)
+    {
+        builder.ToTable("Cards");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .ValueGeneratedNever()
+            .HasColumnType("datetime");
+
+        builder.Property(x => x.UserId)
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100)
+            .HasColumnType("nvarchar(100)");
+
+        builder.HasIndex(x => x.UserId);
+        builder.HasIndex(x => new { x.UserId, x.Name });
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
