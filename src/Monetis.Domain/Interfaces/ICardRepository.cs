1a2a154d83e9e04a643f1216f5d46cf7c87ac774
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-11 17:00:48 -0300

    feat(domain): define ICardRepository interface

---
+++ src/Monetis.Domain/Interfaces/ICardRepository.cs
@@ -0,0 +1,8 @@
+﻿using Monetis.Domain.Entities;
+
+namespace Monetis.Domain.Interfaces;
+
+public interface ICardRepository : IRepository<Card>
+{
+    Task<IEnumerable<Card>> GetByUserReadOnlyAsync(Guid userId);
+}
