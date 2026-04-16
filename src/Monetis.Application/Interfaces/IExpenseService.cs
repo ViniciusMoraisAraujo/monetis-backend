d43d72840a096a0f4f360d275e93f8765dcd3c82
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-16 16:39:41 -0300

    feat: add IExpenseService interface for expense management

---
+++ src/Monetis.Application/Interfaces/IExpenseService.cs
@@ -0,0 +1,19 @@
+using Monetis.Application.DTOs;
+using Monetis.Domain.Entities.Transactions;
+
+namespace Monetis.Application.Interfaces;
+
+public interface IExpenseService
+{
+    Task<ExpenseResponse> CreateExpenseAsync(CreateExpenseRequest request, CancellationToken cancellationToken = default);
+    Task<IEnumerable<ExpenseResponse>> CreateInstallmentAsync(CreateInstallmentRequest request, CancellationToken cancellationToken = default);
+    
+    Task<ExpenseResponse> PayExpenseAsync(Guid expenseId, PayExpenseRequest request, CancellationToken cancellationToken = default);
+    Task<ExpenseResponse> PayInstallmentAsync(Guid expenseId, DateTime paidAt, CancellationToken cancellationToken = default);
+    
+    Task<ExpenseResponse> UpdateExpenseAsync(Guid expenseId, UpdateExpenseRequest request, CancellationToken cancellationToken = default);
+    Task UpdateInstallmentGroupAsync(Guid installmentGroupId, UpdateInstallmentGroupRequest request, CancellationToken cancellationToken = default);
+    
+    Task ProcessOverdueExpensesAsync(CancellationToken cancellationToken = default);
+    
+}
