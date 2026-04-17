38c88eaf7687cd5f1ada9af8889601ebf746697d
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-04-16 22:22:52 -0300

    refactor(application): update IExpenseService interface and remove PayInstallmentAsync

--- src/Monetis.Application/Interfaces/IExpenseService.cs
+++ src/Monetis.Application/Interfaces/IExpenseService.cs
@@ -1,16 +1,13 @@
 using Monetis.Application.DTOs;
-using Monetis.Domain.Entities.Transactions;
 
 namespace Monetis.Application.Interfaces;
 
 public interface IExpenseService
 {
     Task<ExpenseResponse> CreateExpenseAsync(CreateExpenseRequest request, CancellationToken cancellationToken = default);
-    Task<IEnumerable<ExpenseResponse>> CreateInstallmentAsync(CreateInstallmentRequest request, CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<ExpenseResponse>> CreateInstallmentAsync(CreateInstallmentRequest request, CancellationToken cancellationToken = default);
     
     Task<ExpenseResponse> PayExpenseAsync(Guid expenseId, PayExpenseRequest request, CancellationToken cancellationToken = default);
-    Task<ExpenseResponse> PayInstallmentAsync(Guid expenseId, DateTime paidAt, CancellationToken cancellationToken = default);
-    
     Task<ExpenseResponse> UpdateExpenseAsync(Guid expenseId, UpdateExpenseRequest request, CancellationToken cancellationToken = default);
     
     Task ProcessOverdueExpensesAsync(CancellationToken cancellationToken = default);
