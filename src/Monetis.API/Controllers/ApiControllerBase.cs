97f08e2f8ba1f4a2672849a61211fed02b936f3b
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-05-02 20:41:35 -0300

    feat(api): add ApiControllerBase with user ID claim extraction

---
+++ src/Monetis.API/Controllers/ApiControllerBase.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Monetis.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    protected Guid UserId
+    {
+        get
+        {
+            var userIdClaim = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+
+            if (!Guid.TryParse(userIdClaim, out var userId))
+                throw new UnauthorizedAccessException("Invalid user id claim.");
+
+            return userId;
+        }
+    }
+
+    protected bool TryGetUserId(out Guid userId)
+    {
+        var userIdClaim = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+        return Guid.TryParse(userIdClaim, out userId);
+    }
+}
