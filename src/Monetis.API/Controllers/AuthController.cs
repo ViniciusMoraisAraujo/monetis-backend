3f1af31d9708c59d2464daa85225be045beda7e2
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-05-06 13:20:16 -0300

    feat(api): implement AuthController for login

---
+++ src/Monetis.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Monetis.Application.DTOs;
+using Monetis.Application.Interfaces;
+
+namespace Monetis.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IUserAuthService userAuthService) : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(LoginUserRequest loginUserRequest)
+    {
+        var token = await userAuthService.LoginAsync(loginUserRequest);
+        
+        if (token == null)
+            return Unauthorized("Invalid credentials");
+        
+        return Ok(new {Token = token});
+    }
+}
