64a8d488e83fa67f152938860450b072a54f1190
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-05-06 13:22:18 -0300

    feat(infrastructure): configure jwt authentication and authorization

--- src/Monetis.Infrastructure/DependencyInjection.cs
+++ src/Monetis.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Monetis.Application.Interfaces;
 using Monetis.Domain.Interfaces;
 using Monetis.Infrastructure.Contexts;
@@ -29,8 +32,29 @@ public static class DependencyInjection
         services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddScoped<IExpenseRepository, ExpenseRepository>();
         services.AddScoped<ITokenService, TokenService>();
-        
-        
+
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+                };
+            });
+
+        services.AddAuthorization();
         
         return services;
     }
