fe4125935785700f946a8195460d345a17e7775d
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-05-07 21:25:08 -0300

    feat(domain): add transaction exception

--- src/Monetis.Domain/Exceptions/TransactionExceptions.cs
+++ src/Monetis.Domain/Exceptions/TransactionExceptions.cs
@@ -1,3 +1,9 @@
 namespace Monetis.Domain.Exceptions;
 
 public class TransactionInvalidAccountException() : DomainException("Invalid Account Id");
+
+public class TransactionAmountMustBePositiveException() : DomainException("Transaction amount must be greater than zero");
+
+public class TransactionDescriptionRequiredException() : DomainException("Transaction description is required");
+
+public class TransactionDescriptionTooLongException() : DomainException("Transaction description must be less than or equal to 200 characters");
